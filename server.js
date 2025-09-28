e06aad6d6f4e78e6f070fa11eecca136216bb638
kavita <kavitasoren2000@gmail.com>
2025-09-28 11:00:17 +0530

    IELTS Institute

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000
 
 ConnectToMongo()
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}))
 app.use(express.json())
 
 app.get("/", (_req, res) => {
